1a0370699445b21a2904b10840f20381042f314e
songzuozhen_glb <songzuozhen_glb@kgc.com>
2020-11-25 17:18:41 +0800

    asdfsd

--- src/main/java/com/kgc/git076/demo01/controller/TestController.java
+++ src/main/java/com/kgc/git076/demo01/controller/TestController.java
@@ -2,7 +2,10 @@ package com.kgc.git076.demo01.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 public class TestController {
     String name;
     int age;
+    Date birthday;
 }
